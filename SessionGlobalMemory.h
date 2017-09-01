20e3b64a40cff98e02e23b4f57459e8e93bc61f0
me <me@thexp>
2017-09-01 10:57:39 +0900

    add 2013-x64 and tried many fix and failed.

--- SessionGlobalMemory.h
+++ SessionGlobalMemory.h
@@ -4,8 +4,6 @@
 
 #define size_t size_is_not_used_since_its_size_is_different_between_32_and_64
 
-typedef unsigned int size_t32;
-
 #ifdef _DEBUG
 #define AMBIESOFT_VERIFY(e) assert(e)
 #define AMBIESOFT_VERIFY_ZERO(e) assert(0==(e))
@@ -20,299 +18,299 @@ typedef unsigned int size_t32;
 
 namespace Ambiesoft {
 
-template<class T>
-class CSessionGlobalMemory
-{
-	typedef typename CSessionGlobalMemory<T> MYT;
-#ifdef _DEBUG
-	int initialized_;
-#endif
-
-public:
-	explicit CSessionGlobalMemory(LPCSTR pName) {
-#ifdef _DEBUG
-		initialized_ = 0;
-#endif
-		init(pName);
-	}
-	CSessionGlobalMemory(const MYT& rhv)
+	typedef unsigned int size_t32;
+	template<class T>
+	class CSessionGlobalMemory
 	{
+		typedef typename CSessionGlobalMemory<T> MYT;
 #ifdef _DEBUG
-		initialized_ = 0;
+		int initialized_;
 #endif
 
-		init(rhv.m_pName);
-	}
-	MYT& operator=(const MYT& rhv) {
-		if (this != reinterpret_cast<MYT*>(const_cast<MYT*>(&rhv)))
+	public:
+		explicit CSessionGlobalMemory(LPCSTR pName) {
+#ifdef _DEBUG
+			initialized_ = 0;
+#endif
+			init(pName);
+		}
+		CSessionGlobalMemory(const MYT& rhv)
 		{
-			release();
+#ifdef _DEBUG
+			initialized_ = 0;
+#endif
+
 			init(rhv.m_pName);
 		}
-		return *this;
-	}
+		MYT& operator=(const MYT& rhv) {
+			if (this != reinterpret_cast<MYT*>(const_cast<MYT*>(&rhv)))
+			{
+				release();
+				init(rhv.m_pName);
+			}
+			return *this;
+		}
 
 #if !defined(AMBIESOFT_NO_RVALUE_)
-	CSessionGlobalMemory(MYT&& rhv)
-	{
-#ifdef _DEBUG
-		initialized_ = 0;
-#endif
-		move(std::move(rhv));
-#ifdef _DEBUG
-		initialized_ = 1;
-#endif
-	}
-	MYT& operator=(MYT&& rhv) {
-		if (this != reinterpret_cast<MYT*>(&rhv))
+		CSessionGlobalMemory(MYT&& rhv)
 		{
-			release();
+#ifdef _DEBUG
+			initialized_ = 0;
+#endif
 			move(std::move(rhv));
 #ifdef _DEBUG
 			initialized_ = 1;
 #endif
 		}
-		return *this;
-	}
-
-	virtual ~CSessionGlobalMemory() {
-		release();
-	}
-	void move(MYT&& rhv) {
-		m_pName = rhv.m_pName;
-		rhv.m_pName = NULL;
-
-		m_pMutexName = rhv.m_pMutexName;
-		rhv.m_pMutexName = NULL;
-
-		m_ = rhv.m_;
-		rhv.m_ = NULL;
-
-		h_ = rhv.h_;
-		rhv.h_ = NULL;
-
-		p_ = rhv.p_;
-		rhv.p_ = NULL;
-	}
-#endif
-
-	operator T() {
-		ensure();
-		Locker l(m_);
-		//T t;
-		//memcpy(&t, p_, sizeof(t));
-
-		return *((T*)p_);
-	}
-
-	virtual void get(T& t) {
-		ensure();
-		Locker l(m_);
-		memcpy(&t, p_, size());
-	}
-
-	MYT& operator =(const T& t) {
-		set(t);
-		return *this;
-	}
-
-	std::string getName() const {
-		return m_pName;
-	}
-protected:
-	void* p_;
-
-	void init(LPCSTR pName) {
-#ifdef _DEBUG
-		assert(initialized_ == 0);
-		initialized_ = 1;
-#endif
-		h_ = NULL;
-		p_ = NULL;
-		m_ = NULL;
-
-		size_t32 len = lstrlenA(pName);
-		m_pName = (LPSTR)LocalAlloc(LMEM_FIXED, len + sizeof(char));
-		lstrcpyA(m_pName, pName);
-
-		m_pMutexName = (LPSTR)LocalAlloc(LMEM_FIXED, len + sizeof("_Mutex") - 1 + sizeof(char));
-		lstrcpyA(m_pMutexName, pName);
-		lstrcatA(m_pMutexName, "_Mutex");
-	}
-	void release() {
-#ifdef _DEBUG
-		assert(initialized_ == 1);
-		initialized_ = 0;
-#endif
-
-		if (p_)
-		{
-			AMBIESOFT_VERIFY(UnmapViewOfFile(p_));
-			p_ = NULL;
-		}
-
-		if (h_)
-		{
-			AMBIESOFT_VERIFY(CloseHandle(h_));
-			h_ = NULL;
-		}
-
-		if (m_)
-		{
-			AMBIESOFT_VERIFY(CloseHandle(m_));
-			m_ = NULL;
-		}
-		AMBIESOFT_VERIFY_ZERO(LocalFree(m_pName));
-		AMBIESOFT_VERIFY_ZERO(LocalFree(m_pMutexName));
-	}
-	void ensure() {
-		if (!m_)
-		{
-			m_ = CreateMutexA(NULL, FALSE, m_pMutexName);
-			assert(m_);
-		}
-
-		bool first = false;
-		if (!h_)
-		{
-			h_ = CreateFileMappingA(
-				INVALID_HANDLE_VALUE,
-				NULL,
-				PAGE_READWRITE,
-				0, internalsize(),
-				m_pName);
-			assert(h_);
-			if (h_ && GetLastError() != ERROR_ALREADY_EXISTS)
+		MYT& operator=(MYT&& rhv) {
+			if (this != reinterpret_cast<MYT*>(&rhv))
 			{
-				first = true;
+				release();
+				move(std::move(rhv));
+#ifdef _DEBUG
+				initialized_ = 1;
+#endif
 			}
-		}
-		if (!p_)
-		{
-			p_ = MapViewOfFile(
-				h_,
-				FILE_MAP_READ | FILE_MAP_WRITE,
-				0, 0,
-				internalsize());
-			assert(p_);
+			return *this;
 		}
 
-		if (first)
-		{
+		virtual ~CSessionGlobalMemory() {
+			release();
+		}
+		void move(MYT&& rhv) {
+			m_pName = rhv.m_pName;
+			rhv.m_pName = NULL;
+
+			m_pMutexName = rhv.m_pMutexName;
+			rhv.m_pMutexName = NULL;
+
+			m_ = rhv.m_;
+			rhv.m_ = NULL;
+
+			h_ = rhv.h_;
+			rhv.h_ = NULL;
+
+			p_ = rhv.p_;
+			rhv.p_ = NULL;
+		}
+#endif
+
+		operator T() {
+			ensure();
 			Locker l(m_);
+			//T t;
+			//memcpy(&t, p_, sizeof(t));
+
+			return *((T*)p_);
+		}
+
+		virtual void get(T& t) {
+			ensure();
+			Locker l(m_);
+			memcpy(&t, p_, size());
+		}
+
+		MYT& operator =(const T& t) {
+			set(t);
+			return *this;
+		}
+
+		std::string getName() const {
+			return m_pName;
+		}
+	protected:
+
+		void init(LPCSTR pName) {
+#ifdef _DEBUG
+			assert(initialized_ == 0);
+			initialized_ = 1;
+#endif
+			h_ = NULL;
+			p_ = NULL;
+			m_ = NULL;
+
+			size_t32 len = lstrlenA(pName);
+			m_pName = (LPSTR)LocalAlloc(LMEM_FIXED, len + sizeof(char));
+			lstrcpyA(m_pName, pName);
+
+			m_pMutexName = (LPSTR)LocalAlloc(LMEM_FIXED, len + sizeof("_Mutex") - 1 + sizeof(char));
+			lstrcpyA(m_pMutexName, pName);
+			lstrcatA(m_pMutexName, "_Mutex");
+		}
+		void release() {
+#ifdef _DEBUG
+			assert(initialized_ == 1);
+			initialized_ = 0;
+#endif
+
+			if (p_)
+			{
+				AMBIESOFT_VERIFY(UnmapViewOfFile(p_));
+				p_ = NULL;
+			}
+
+			if (h_)
+			{
+				AMBIESOFT_VERIFY(CloseHandle(h_));
+				h_ = NULL;
+			}
+
+			if (m_)
+			{
+				AMBIESOFT_VERIFY(CloseHandle(m_));
+				m_ = NULL;
+			}
+			AMBIESOFT_VERIFY_ZERO(LocalFree(m_pName));
+			AMBIESOFT_VERIFY_ZERO(LocalFree(m_pMutexName));
+		}
+		void ensure() {
+			if (!m_)
+			{
+				m_ = CreateMutexA(NULL, FALSE, m_pMutexName);
+				assert(m_);
+			}
+
+			bool first = false;
+			if (!h_)
+			{
+				h_ = CreateFileMappingA(
+					INVALID_HANDLE_VALUE,
+					NULL,
+					PAGE_READWRITE,
+					0, internalsize(),
+					m_pName);
+				assert(h_);
+				if (h_ && GetLastError() != ERROR_ALREADY_EXISTS)
+				{
+					first = true;
+				}
+			}
+			if (!p_)
+			{
+				p_ = MapViewOfFile(
+					h_,
+					FILE_MAP_READ | FILE_MAP_WRITE,
+					0, 0,
+					internalsize());
+				assert(p_);
+			}
+
 			if (first)
 			{
-				memset(p_, 0, internalsize());
-				first = false;
+				Locker l(m_);
+				if (first)
+				{
+					memset(p_, 0, internalsize());
+					first = false;
+				}
 			}
 		}
-	}
 
-	//T get() {
-	//}
+		//T get() {
+		//}
 
 
-	virtual void set(const T& t) {
-		ensure();
-		Locker l(m_);
-		memcpy(p_, &t, size());
-	}
-
-	virtual size_t32 size() const {
-		return sizeof(T);
-	}
-	virtual size_t32 internalsize() const {
-		return size();
-	}
-
-	class Locker {
-	public:
-		HANDLE m_;
-		Locker(HANDLE m) {
-			m_ = m;
-			WaitForSingleObject(m, INFINITE);
+		virtual void set(const T& t) {
+			ensure();
+			Locker l(m_);
+			memcpy(p_, &t, size());
 		}
-		~Locker() {
-			AMBIESOFT_VERIFY(ReleaseMutex(m_));
+
+		virtual size_t32 size() const {
+			return sizeof(T);
+		}
+		virtual size_t32 internalsize() const {
+			return size();
+		}
+
+		class Locker {
+		public:
+			HANDLE m_;
+			Locker(HANDLE m) {
+				m_ = m;
+				WaitForSingleObject(m, INFINITE);
+			}
+			~Locker() {
+				AMBIESOFT_VERIFY(ReleaseMutex(m_));
+			}
+		};
+
+	protected:
+		bool first_;
+		HANDLE h_;
+		HANDLE m_;
+		LPSTR m_pName;
+		LPSTR m_pMutexName;
+		void* p_;
+		// prohibitted, use NTS
+		// T* operator &() {}
+	};
+
+	// Non Thread Safe
+	template<class T>
+	class CSessionGlobalMemoryNTS : CSessionGlobalMemory<T>
+	{
+	public:
+		explicit CSessionGlobalMemoryNTS(LPCSTR pName) : CSessionGlobalMemory<T>(pName) {
+
+		}
+		T* operator &() {
+			ensure();
+			return (T*)p_;
 		}
 	};
 
 
-	bool first_;
-	HANDLE h_;
-	HANDLE m_;
-	LPSTR m_pName;
-	LPSTR m_pMutexName;
-
-	// prohibitted, use NTS
-	// T* operator &() {}
-};
-
-// Non Thread Safe
-template<class T>
-class CSessionGlobalMemoryNTS : CSessionGlobalMemory<T>
-{
-public:
-	explicit CSessionGlobalMemoryNTS(LPCSTR pName) : CSessionGlobalMemory<T>(pName) {
-
-	}
-	T* operator &() {
-		ensure();
-		return (T*)p_;
-	}
-};
-
-
-class CDynamicSessionGlobalMemory : public CSessionGlobalMemory<size_t32>
-{
-private:
-	size_t32 size_;
-public:
-	// creator
-	explicit CDynamicSessionGlobalMemory(LPCSTR pName, size_t32 size) : CSessionGlobalMemory<size_t32>(pName) {
-		size_ = size;
-	}
-
-	// user
-	explicit CDynamicSessionGlobalMemory(LPCSTR pName) : CSessionGlobalMemory<size_t32>(pName) {
-		size_ = -1;
-	}
-
-	~CDynamicSessionGlobalMemory() {
-
-	}
-	size_t32 size() const {
-		if (size_ == -1)
-		{
-			CDynamicSessionGlobalMemory tmp(m_pName, sizeof(size_));
-			tmp.internalget(const_cast<size_t32*>(&size_));
+	class CDynamicSessionGlobalMemory : public CSessionGlobalMemory<size_t32>
+	{
+	private:
+		size_t32 size_;
+	public:
+		// creator
+		explicit CDynamicSessionGlobalMemory(LPCSTR pName, size_t32 size) : CSessionGlobalMemory<size_t32>(pName) {
+			size_ = size;
 		}
-		return size_;
-	}
-	virtual size_t32 internalsize() const {
-		return sizeof(size_) + size();
-	}
-	void get(unsigned char* p) {
-		ensure();
-		Locker l(m_);
-		memcpy(p, (unsigned char*)(p_) + sizeof(size_), size());
-	}
-	void internalget(size_t32* ps) {
-		ensure();
-		Locker l(m_);
-		*ps = *(size_t32*)p_;
-	}
 
-	void set(const unsigned char* p) {
-		// user can not set before get
-		assert(size_ != -1);
+		// user
+		explicit CDynamicSessionGlobalMemory(LPCSTR pName) : CSessionGlobalMemory<size_t32>(pName) {
+			size_ = -1;
+		}
 
-		ensure();
-		Locker l(m_);
-		*(size_t32*)p_ = size();
-		memcpy((unsigned char*)(p_) + sizeof(size_), p, size());
-	}
-};
+		~CDynamicSessionGlobalMemory() {
+
+		}
+		size_t32 size() const {
+			if (size_ == -1)
+			{
+				CDynamicSessionGlobalMemory tmp(m_pName, sizeof(size_));
+				tmp.internalget(const_cast<size_t32*>(&size_));
+			}
+			return size_;
+		}
+		virtual size_t32 internalsize() const {
+			return sizeof(size_) + size();
+		}
+		void get(unsigned char* p) {
+			ensure();
+			Locker l(m_);
+			memcpy(p, (unsigned char*)(p_)+sizeof(size_), size());
+		}
+		void internalget(size_t32* ps) {
+			ensure();
+			Locker l(m_);
+			*ps = *(size_t32*)p_;
+		}
+
+		void set(const unsigned char* p) {
+			// user can not set before get
+			assert(size_ != -1);
+
+			ensure();
+			Locker l(m_);
+			*(size_t32*)p_ = size();
+			memcpy((unsigned char*)(p_)+sizeof(size_), p, size());
+		}
+	};
 
 
 
